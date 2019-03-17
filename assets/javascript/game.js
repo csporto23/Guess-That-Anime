d3a35bf86cfc28babd7c3fd5fb250ea4fb99d5b9
csporto23 <Csporto23@gmail.com>
2019-03-17 16:44:21 -0500

    trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,10 @@ alert ("You have 10 guesses to guess the word/title of the anime")
 
 GamepadButton.onclick = document.getElementById("imgGuess")
 
-const Answer = "Naruto"
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+const word = ['Naruto', 'DragonBallZ', 'FullMetalAlchemist']
 
 
 
